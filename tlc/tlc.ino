3f99596bb1564fb46d330395f849149c7caa4e16
Jorge Lopez <jl5659@gmail.com>
2019-06-20 21:58:54 -0500

    Let's see

--- tlc/tlc.ino
+++ tlc/tlc.ino
@@ -1,68 +1,14 @@
 #include <FastLED.h>
 #include <math.h>
-#define LED_PIN     13
-#define NUM_LEDS    150
-#define POT1_PIN    0
-#define BUTTON1_PIN  12
+#define LED_PIN 13
+#define NUM_LEDS 150
+#define POT1_PIN 0
+#define BUTTON1_PIN 12
 #define DEBUG 1
 
 CRGB leds[NUM_LEDS];
 
-class TotallyLegitController {
-   public:
-    void setup() {
-      Serial.print("set...");
-    }
-
-    void loop(CRGB cleds[NUM_LEDS], float basePercent) {
-
-
-      float widthPercent = .15;
-      int dashNum = 4;
-
-      int dashOffset = dashNum;
-      float ledWidth = widthPercent * NUM_LEDS;
-    
-     
-      for (int i = 0; i < NUM_LEDS; i++) {
-        int hue = (float)i/NUM_LEDS * 255;
-
-        cleds[i] = CHSV(255, 255, 0);
-
-        for (int j = 0 - dashOffset; j < dashNum + dashOffset; j++) {
-
-           float dashOffset = ((float) j / dashNum) * NUM_LEDS;
-           float selectedLed = (basePercent * NUM_LEDS) + dashOffset;
-  
-           if (abs(i-selectedLed) < (ledWidth / 2)) {
-            float distPercent = abs(i-selectedLed) / (ledWidth / 2);
-            float intensity = cos(PI * .5 * distPercent) * 255;
-            cleds[i] = CHSV(hue, 255-(intensity/2), intensity*.75);
-            }
-        }
-          
-       }
-       
-//       int hue = (float)i/NUM_LEDS * 255;
-//         
-//        // Default off.
-//        cleds[i] = CHSV(hue, 255, 0);  
-//
-//        abs(i-selectedLed) < (ledWidth / 2)
-//        
-//        if (abs(i-selectedLed) < (ledWidth / 2)) {
-//
-//          float distPercent = abs(i-selectedLed) / (ledWidth / 2);
-//          float intensity = cos(PI * .5 * distPercent) * 255;
-//          
-//          cleds[i] = CHSV(hue, 255, 255);  
-//        }
-
-    }
-};
-
 TotallyLegitController tlc;
-
 
 int offset = 0;
 
