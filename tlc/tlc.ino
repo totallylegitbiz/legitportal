e1424949e2c4fe65ad952054c15bea4bb45f76f2
Jorge Lopez <jl5659@gmail.com>
2019-06-20 23:04:43 -0500

    It spins

--- tlc/tlc.ino
+++ tlc/tlc.ino
@@ -16,6 +16,7 @@ CRGB leds[NUM_LEDS];
 Dashspin dashspin;
 
 int offset = 0;
+int velocity = 0;
 
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
@@ -23,23 +24,36 @@ void setup() {
   Serial.print("Starting...");
 
   pinMode(BUTTON1_PIN, INPUT);
-
-  // Setup dashsin here
-  // dashpin.config
   
-//  tlc.setup();
-
-//  dashspin.setup(NUM_LEDS);
-
 }
 
 void loop() {
   
   int button1State = digitalRead(BUTTON1_PIN);
   float pot1Percent = (float) analogRead(POT1_PIN) / 1024;
+
+  velocity = (pot1Percent-.5) * 500;
+  
+  Serial.print("velocity...");
+  Serial.print(velocity);
+  Serial.print("\n");
   
   int cycleMs = 5 * 1000;
-  int offset = millis() % cycleMs;
+  
+  int nextOffset = velocity + offset;
+
+  if (nextOffset >= 0) {
+     offset = nextOffset % cycleMs;
+  } else {
+     offset = cycleMs - (nextOffset % cycleMs);
+  }
+  
+
+ 
+
+  Serial.print("offset...");
+  Serial.print(offset);
+  Serial.print("\n");
   
   float basePercent = (float) offset / cycleMs;
   
