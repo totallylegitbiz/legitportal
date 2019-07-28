e65ebcb1df17be9a2fd2e1df2765fdbf90ec824f
Jorge Lopez <jl5659@gmail.com>
2019-07-28 12:12:01 -0500

    Lots of lights

---
+++ lib/Effects/Effects/Throb.h
@@ -0,0 +1,14 @@
+#include <EffectTypes.h>
+
+void throbEffectLoop(struct EffectState *effectState, int hue)
+{
+    const unsigned int loopMs = 3000;
+    const unsigned int loopPosition = effectState->loopPosition % loopMs;
+    const float loopPercent = float(loopPosition) / loopMs;
+    const unsigned int intensity = abs(cos(PI * .5 * loopPercent * 2) * 255);
+
+    for (int i = 0; i < LED_CNT; i++)
+    {
+        leds[i] = CHSV(hue, 255, intensity);
+    }
+}
