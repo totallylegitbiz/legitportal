eb5ad839c73a995533aea6e7864b5c6e29c7a8b7
Jorge Lopez <jl5659@gmail.com>
2019-08-05 21:20:34 -0500

    Adding hue sparkle

--- lib/Effects/Effects/HueSparkle.h
+++ lib/Effects/Effects/HueSparkle.h
@@ -7,7 +7,7 @@ void hueSparkleEffectLoop(struct EffectState *effectState)
    for (int i = 0; i < LED_CNT; i++)
   {
 
-    leds[i] = CHSV(0, 244, 255);
+    leds[i] = CHSV(140, 244, 255);
   }
 
 }
